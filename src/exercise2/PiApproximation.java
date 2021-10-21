04dcda980f6875660c96b4749e32a54f428e3cae
victory310 <>
2021-10-21 19:06:23 +0000

    Files updated

--- src/exercise2/PiApproximation.java
+++ src/exercise2/PiApproximation.java
@@ -1,18 +1,9 @@
 package exercise2;
 
-<<<<<<< HEAD
 public class PiApproximation {
 
     public static void main(String[] args) {
       double fiki = 4.0 * (1.0 - 1.0/3.0 + 1.0/5.0 - 1.0/7.0 + 1.0/11.0 - 1.0/13.0 + 1.0/17.0 - 1.0/19.0 + 1.0/23.0);
-=======
-import java.util.Scanner;
-
-public class PiApproximation {
-
-    public static void main(String[] args) {
-      double fiki = 4.0 *(1.0 - 1.0/3.0 + 1.0/5.0 - 1.0/7.0 + 1.0/11.0 - 1.0/13.0 + 1.0/17.0 - 1.0/19.0 + 1.0/23.0);
->>>>>>> 42f3869c9f4b527627b8e723d7b4b98131181b27
       System.out.println ("Pi:" + fiki);
     }
 
